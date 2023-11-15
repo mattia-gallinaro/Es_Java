907e160f783586f0482fe26a1c7a7edbfeaff330
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2023-11-15 12:21:35 +0100

    2 righe in più

--- SimpleTriangoloTester.java
+++ SimpleTriangoloTester.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class SimpleTriangoloTester{
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
+	System.out.println("inserisci tre numeri es 3 4 5");
+	int num1 = Integer.parseInt(sc.next());
+	int num2 = Integer.parseInt(sc.next());
+	int num3 = Integer.parseInt(sc.next());
 	}
 }
