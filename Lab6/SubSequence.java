75408d088655565d94e38eadecadf5074bcf33a7
mattia-gallinaro <gallinaromattia6@gmail.com>
2023-11-29 22:17:14 +0100

    corretto es 4 lab 6

--- Lab6/SubSequence.java
+++ Lab6/SubSequence.java
@@ -3,28 +3,33 @@ import java.util.Scanner;
 public class SubSequence{
 	public static void main(String[] args){
 		String s1 = "", s2 = "";
-		boolean flag =  true;
-		int last_point = 0;
+		boolean flag =  true , letter_found = false;
+		int last_point = -1, count =0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Inserisci la prima stringa");
 		s1 = sc.nextLine();
 		System.out.println("Inserisci la seconda stringa");
 		s2 = sc.nextLine();
+
 		for(int i = 0; i < s2.length(); i++){
-			for(last_point = last_point; last_point < s1.length() ;last_point++){
-				if(s1.charAt(last_point) == s2.charAt(i)){
+			letter_found = false;
+			for(int j = last_point + 1; j < s1.length() ;j++){
+				if(s1.charAt(j) == s2.charAt(i)){
+					last_point = j;
+					count += 1;
+					letter_found = true;
 					break;
 				}
 			}
-			if((s1.length() - last_point) < (s2.length() - i)){
+			if((s1.length() - last_point - 1) < (s2.length() - count -1) || letter_found == false ){
 				flag = false;
 				break;
 			}
 		}
 		if(flag){
-			System.out.println("Sono sottostringhe");
+			System.out.println("Sono sottosequenze");
 		}else{
-			System.out.println("Non sono sottostringhe");
+			System.out.println("Non sono sottosequenze");
 		}
 		
 	}
